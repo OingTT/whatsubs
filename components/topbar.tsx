28cc95e13f31d84fb204bbd33fc74830e441172b
Joohyung Park <johy@dabyeol.com>
2023-03-13 21:21:00 +0900

    Improve topbar

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -4,105 +4,59 @@ import Link from "next/link";
 import { MagnifyingGlass } from "@phosphor-icons/react";
 
 const Bar = styled.div`
-  box-sizing: border-box;
   width: 100%;
   height: 64px;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
   align-items: center;
   padding: 32px;
   box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.1);
   background-color: #ffffff;
-  overflow: hidden;
-  align-content: center;
-  position: absolute;
+  gap: 24px;
+  position: fixed;
+  top: 0;
 `;
 
-const Logo = styled(Image)`
-  flex-shrink: 0;
-  display: block;
-  overflow: visible;
-  position: relative;
-  aspect-ratio: 1 / 1;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  border-radius: 24px;
-  margin-right: 32px;
+const Nav = styled(Link)`
+  color: #666;
+  font-weight: 400;
 `;
 
-const About = styled.div`
-  flex-shrink: 0;
-  width: fit-content; /* 46px */
-  height: auto; /* 32px */
-  white-space: pre;
-  overflow: visible;
-  position: relative;
-  line-height: 1.2;
-  margin-right: 32px;
+const Spacer = styled.div`
+  flex: 1;
 `;
 
-const Login = styled.div`
-  flex-shrink: 0;
-  width: fit-content; /* 41px */
-  height: auto; /* 32px */
-  white-space: pre;
-  overflow: visible;
-  position: relative;
-  line-height: 1.2;
-  margin-left: auto;
-`;
-
-const Columns = styled.div`
-  flex-shrink: 0;
-  flex: 1 0 0px;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 10px;
-`;
-
-const User = styled.div`
-  flex-shrink: 0;
-  width: 32px;
-  height: 32px;
-  display: block;
+const User = styled(Image)`
   background-color: #333333;
-  overflow: visible;
-  position: relative;
-  aspect-ratio: 1 / 1;
-  border-radius: 16px;
-  margin-left: 32px;
-  margin-right: 0;
+  border-radius: 100%;
 `;
 
 export default function Topbar() {
   return (
     <Bar>
-      <Link href={"/"} passHref>
-        <Logo
+      <Link href="/">
+        <Image
           src={`/images/whatsubs-small.png`}
-          width="32"
-          height="32"
-          alt="logo"
+          width="24"
+          height="24"
+          alt="Logo"
         />
       </Link>
-      <Link href={"/"} passHref>
-        <About>About</About>
-      </Link>
-      <Link href={"/login"} passHref>
-        <Login>Login</Login>
-      </Link>
 
-      <Columns></Columns>
+      <Nav href="/about">About</Nav>
+      <Nav href="/login">Login</Nav>
 
-      <Link href={"/"} passHref>
-        <MagnifyingGlass size={32}></MagnifyingGlass>
+      <Spacer />
+
+      <Link href="/search">
+        <MagnifyingGlass color="#333" size="24" />
       </Link>
-      <Link href={"/"} passHref>
-        <User />
+      <Link href="/user">
+        <User
+          src="/images/whatsubs-dark.png"
+          width="32"
+          height="32"
+          alt="User"
+        />
       </Link>
     </Bar>
   );
