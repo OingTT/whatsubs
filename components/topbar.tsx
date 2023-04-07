e96e0626814b8c3870f1cf01db736bb213cf5b48
Joohyung Park <johy@dabyeol.com>
2023-04-08 00:13:42 +0900

    Add user page

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import Image from "next/image";
 import Link from "next/link";
 import { MagnifyingGlass } from "@phosphor-icons/react";
+import useUser from "@/lib/client/useUser";
 
 const Bar = styled.div`
   width: 100%;
@@ -26,16 +27,17 @@ const Spacer = styled.div`
 `;
 
 const User = styled(Image)`
-  background-color: #333333;
   border-radius: 100%;
 `;
 
 export default function Topbar() {
+  const user = useUser();
+
   return (
     <Bar>
       <Link href="/">
         <Image
-          src={`/images/whatsubs-small.png`}
+          src="/images/whatsubs-small.png"
           width="24"
           height="24"
           alt="Logo"
@@ -51,7 +53,7 @@ export default function Topbar() {
       </Link>
       <Link href="/user">
         <User
-          src="/images/whatsubs-dark.png"
+          src={user?.image || "/images/whatsubs-dark.png"}
           width="32"
           height="32"
           alt="User"
