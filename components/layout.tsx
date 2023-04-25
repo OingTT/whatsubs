866de475aa09fe37c7ff437597ebb8e3cfba8d66
Joohyung Park <johy@dabyeol.com>
2023-04-26 00:25:02 +0900

    Improve movie details page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,22 +1,23 @@
 import styled from "@emotion/styled";
 import Topbar from "./topbar";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ fit?: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  padding: 72px 0px 0px 0px;
+  padding: ${(props) => (props.fit ? "64px 0px 0px 0px" : "72px 0px 0px 0px")};
   gap: 8px;
 `;
 
 interface LayoutProps {
+  fit?: boolean;
   children?: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ fit, children }: LayoutProps) {
   return (
-    <Wrapper>
+    <Wrapper fit={fit}>
       <Topbar />
       {children}
     </Wrapper>
