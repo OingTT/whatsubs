20b125a9ea7aaa4f6b7195169bc3fbd8616d0742
skjy1998 <skjy1998@gmail.com>
2023-05-11 03:58:09 +0900

    ActorTVSearch

--- pages/search.tsx
+++ pages/search.tsx
@@ -9,6 +9,17 @@ interface Movie {
   poster_path: string;
 }
 
+interface Actor {
+  id: number;
+  name: string;
+}
+
+interface TVShow {
+  id: number;
+  name: string;
+  poster_path: string;
+}
+
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
@@ -34,14 +45,22 @@ const SearchBox = styled.div`
   align-items: center;
 `;
 const Input = styled.input`
+  box-sizing: border-box;
+  flex-shrink: 0;
   width: 640px;
   height: 40px;
-  flex: 1;
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: none;
-  border-bottom: 2px solid #ccc;
-  outline: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 16px 16px 16px;
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  overflow: hidden;
+  flex: 1 0 0px;
+  position: relative;
+  align-content: center;
+  flex-wrap: nowrap;
 
   &:focus {
     border-color: #007bff;
@@ -85,9 +104,16 @@ const Title = styled.h2`
   margin: 0;
 `;
 
+const ActorInfo = styled.div`
+  margin-top: 20px;
+  font-size: 18px;
+`;
+
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [actor, setActor] = useState<Actor | null>(null);
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [tvShows, setTVShows] = useState<TVShow[]>([]);
 
   const handleSearchTermChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -96,9 +122,47 @@ export default function Search() {
   };
 
   const handleSearch = async () => {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&query=${searchTerm}`;
+    const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&region=KR&query=${searchTerm}`;
+    const searchActorUrl = `https://api.themoviedb.org/3/search/person?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&region=KR&query=${searchTerm}`;
+    const searchTVShowUrl = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&region=KR&query=${searchTerm}`;
+
+    const [actorResponse, movieResponse, tvShowResponse] = await Promise.all([
+      axios.get(searchActorUrl),
+      axios.get(searchMovieUrl),
+      axios.get(searchTVShowUrl),
+    ]);
+
+    const actorData = actorResponse.data.results[0];
+    if (actorData) {
+      const actorInfo: Actor = {
+        id: actorData.id,
+        name: actorData.name,
+      };
+      setActor(actorInfo);
+      fetchMovies(actorInfo.id);
+      fetchTVShows(actorInfo.id);
+    } else {
+      setActor(null);
+      setMovies([]);
+      setTVShows([]);
+    }
+    const movieData = movieResponse.data.results;
+    setMovies(movieData);
+
+    const tvShowData = tvShowResponse.data.results;
+    setTVShows(tvShowData);
+  };
+
+  const fetchMovies = async (actorId: number) => {
+    const url = `https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&region=KR`;
     const response = await axios.get(url);
-    setMovies(response.data.results);
+    setMovies(response.data.cast);
+  };
+
+  const fetchTVShows = async (actorId: number) => {
+    const url = `https://api.themoviedb.org/3/person/${actorId}/tv_credits?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&region=KR`;
+    const response = await axios.get(url);
+    setTVShows(response.data.cast);
   };
 
   return (
@@ -112,13 +176,26 @@ export default function Search() {
           />
           <Button onClick={handleSearch}>검색</Button>
         </SearchBox>
+        {actor && <h3>{actor.name}</h3>}
         <MovieList>
+          <h2>영화</h2>
           {movies.map((movie) => (
             <MovieItem key={movie.id}>
               <Poster
                 src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
               />
               <Title>{movie.title}</Title>
+            </MovieItem>
+          ))}
+        </MovieList>
+        <MovieList>
+          <h2>tv</h2>
+          {tvShows.map((tvShow) => (
+            <MovieItem key={tvShow.id}>
+              <Poster
+                src={`https://image.tmdb.org/t/p/w200${tvShow.poster_path}`}
+              />
+              <Title>{tvShow.name}</Title>
             </MovieItem>
           ))}
         </MovieList>
