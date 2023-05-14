153fcde2ee44910a164ec08b149928c65ec75a02
Joohyung Park <johy@dabyeol.com>
2023-05-14 16:47:51 +0900

    Add policies

---
+++ pages/policy/[slug].tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+import matter from "gray-matter";
+import { GetStaticProps } from "next";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  gap: 24px;
+
+  @media (min-width: 1200px) {
+    width: 984px;
+  }
+
+  @media (max-width: 809px) {
+    padding: 16px;
+    gap: 16px;
+  }
+`;
+
+interface PolicyProps {
+  data: {
+    title: string;
+    date: string;
+  };
+  content: string;
+}
+
+export default function Policy({ data, content }: PolicyProps) {
+  return (
+    <Wrapper>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </Wrapper>
+  );
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { content, data } = matter.read(
+    `documents/policy/${ctx.params?.slug}.md`
+  );
+  const { value } = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(content);
+
+  return {
+    props: {
+      data,
+      content: value,
+    },
+  };
+};
